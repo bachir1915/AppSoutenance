bd4da5d0858e25aef0c29d2a5e35086c0d672b68
bachir1915 <bachirbousso10@gmail.com>
2026-01-11 02:58:38 +0000

    Initial commit: Add SenSoutenances project

---
+++ SenSoutenances/Views/Parametre/frmSession.cs
@@ -0,0 +1,89 @@
+﻿using SenSoutenance.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZstdSharp.Unsafe;
+
+namespace SenSoutenance.Views.Parametre
+{
+    public partial class frmSession : Form
+    {
+        public frmSession()
+        {
+            InitializeComponent();
+        }
+
+        BdSenSoutenanceContext db = new BdSenSoutenanceContext();
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = int.Parse(dgSession.CurrentRow.Cells[0].Value.ToString());
+                Session session = db.sessions.Find(id);
+                if (session != null)
+                {
+                    session.LibelleSession = txtSession.Text;
+                    session.IdAnneeAcademique = (int?)cbbAnneeAcademique.SelectedValue;
+                    db.SaveChanges();
+                    Effacer();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de la modification : " + ex.Message);
+            }
+        }
+
+        private void frmSession_Load(object sender, EventArgs e)
+        {
+            Effacer();
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            Session session = new Session()
+            {
+                LibelleSession = txtSession.Text,
+                IdAnneeAcademique = (int?)cbbAnneeAcademique.SelectedValue
+            };
+            db.sessions.Add(session);
+            db.SaveChanges();
+            Effacer();
+        }
+
+        private void Effacer()
+        {
+            txtSession.Clear();
+            cbbAnneeAcademique.SelectedValue = null;
+            dgSession.DataSource = db.sessions.ToList();
+            cbbAnneeAcademique.DataSource = db.anneeAcademiques.ToList();
+            cbbAnneeAcademique.DisplayMember = "LibelleAnneeAcademique";
+            cbbAnneeAcademique.ValueMember = "IdAnneeAcademique";
+            txtSession.Focus();
+        }
+
+        private void btnSelectionner_Click(object sender, EventArgs e)
+        {
+            int? id = int.Parse(dgSession.CurrentRow.Cells[0].Value.ToString());
+            Session session = db.sessions.Find(id);
+            txtSession.Text = session.LibelleSession;
+            cbbAnneeAcademique.SelectedValue = session.IdAnneeAcademique;
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(dgSession.CurrentRow.Cells[0].Value.ToString());
+            Session session = db.sessions.Find(id);
+            db.sessions.Remove(session);
+            db.SaveChanges();
+            Effacer();
+        }
+    }
+}
